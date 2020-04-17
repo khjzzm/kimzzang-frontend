a3a9b9735561b285e2116285b65b2cf3a4acab28
김현진 <khjzzm@gmail.com>
2020-04-17 09:52:54 +0900

    delete resetcss component

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ResetCss from '../components/reset-css';
 import Layout from '../components/layout'
 import SEO from '../components/seo';
 import NotFound from '../components/page/not-found';
@@ -7,7 +6,6 @@ import NotFound from '../components/page/not-found';
 const NotFoundPage = (props) => {
     return (
         <Layout>
-            <ResetCss />
             <SEO title="404: Not Found" />
             <NotFound />
         </Layout>
